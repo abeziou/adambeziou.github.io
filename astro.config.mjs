6383f8406287c12e5ab6a7c1d817027d87029cf5
Adam Beziou <adam.beziou@gmail.com>
2026-09-11 00:34:14 -0600

    feat: add sanity cms support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+
 export default defineConfig({
-    site: 'https://abeziou.dev',
+  site: 'https://abeziou.dev',
+  integrations: [
+    sanity({
+      projectId: "japwn6x8",
+      dataset: "production",
+      useCdn: false,
+    }),
+  ],
 });
